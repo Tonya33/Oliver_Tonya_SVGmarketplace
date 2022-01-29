c992709f51859d5f6d96578689f205c9f9f130c3
Tonya Oliver <tonya.anne.oliver@gmail.com>
2022-01-29 18:09:10 -0500

    finishing up layout

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 (() => {
+  //this is js comment
+  console.log('this is some text');
 
-  console.log('Welcome to Girl Power SVG Store');
+  //go into the document and make a reference to the element that you want to work with
+  let theHeading = document.querySelector(".main-heading"),
+      theIcons = document.querySelectorAll(".icon");
 
-  let theHeading = document.querySelector("main-heading");
-
+//create a function that runs when theHeader is clicked
   function logElement() {
-    console.log('clicked on the element', this.id);
+    console.log('clicked on the element:', this.id);
   }
 
-  theHeading.addEventListener('click', logElement);
+//listen for a oser event and something
+//the do something is the function we run when the element is clicked
+  theHeading.addEventListener("click", logElement);
+  /*this is a block comment
+  that can run on multiple lines*/
+
+//loop through all of the icons and add event handling
+//process each icon at a time and add behaviour
+  theIcons.forEach(icon => icon.addEventListener('click', logElement));
 
 })();
